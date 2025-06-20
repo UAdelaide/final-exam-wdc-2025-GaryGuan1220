2fbfd55f45cd522a9c7dff86c241d4798fe94d7a
GaryGuan1220 <guanycaa@163.com>
2025-06-20 08:07:58 +0000

    [automatic commit] 2025-06-20 08:07:58 UTC 129.127.202.63

---
+++ part1/routes/walkers.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const pool = require("../db");
+
+router.get('/', async (req, res, next) => {
+    try {
+        const [rows] = await pool.query(`
+            SELECT WalkRequests.*, Dogs.name AS dog_name
+            FROM WalkRequests
+            JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+            WHERE status = 'open'
+        `);
+        res.json(rows);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
