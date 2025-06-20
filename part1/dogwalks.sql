18869a9d9020b02f4a4353ba94b6373960edf0a8
GaryGuan1220 <guanycaa@163.com>
2025-06-20 07:11:14 +0000

    [automatic commit] 2025-06-20 07:11:14 UTC 129.127.202.63

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -69,7 +69,9 @@ INSERT INTO Dogs(name, size, owner_id, role) VALUES
 ('Shai','small', (SELECT user_id FROM Users WHERE username = 'gary123'));
 
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT user_id FROM Users WHERE username = 'Max'),'2025-06-10 08:00:00','','',''),
+(
+    (SELECT user_id FROM Users WHERE username = 'Max'),'2025-06-10 08:00:00','','',''
+),
 ((SELECT user_id FROM Users WHERE username = 'Bella'),'2025-06-10 09:30:00','','',''),
 ((SELECT user_id FROM Users WHERE username = 'alice123'),'','','',''),
 ((SELECT user_id FROM Users WHERE username = 'alice123'),'','','',''),
