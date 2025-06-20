b15037e34918d895cd790a30e432f6d6b763338a
GaryGuan1220 <guanycaa@163.com>
2025-06-20 07:03:20 +0000

    [automatic commit] 2025-06-20 07:03:20 UTC 129.127.202.63

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -55,11 +55,11 @@ CREATE TABLE WalkRatings (
 );
 
 INSERT INTO Users(username, email, password_hash, role) VALUES
-('alice123','','',''),
-('bobwalker','','',''),
-('alice123','','',''),
-('alice123','','',''),
-('alice123','','','');
+('alice123','alice@example.com','',''),
+('bobwalker','bob@example.com','',''),
+('carol123','','',''),
+('gary123','','',''),
+('gtq123','','','');
 Five users:
 A user with the username alice123, email alice@example.com, password hash hashed123, and role owner.
 A user with the username bobwalker, email bob@example.com, password hash hashed456, and role walker.
