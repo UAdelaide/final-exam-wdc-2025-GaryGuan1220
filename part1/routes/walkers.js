50b376de9196c6f4f4203be3c837a85a07b9d103
GaryGuan1220 <guanycaa@163.com>
2025-06-20 12:10:29 +0000

    [automatic commit] 2025-06-20 12:10:29 UTC 119.15.78.5

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -5,12 +5,13 @@ const pool = require("../db");
 router.get('/', async (req, res, next) => {
     try {
         const [rows] = await pool.query(`
-            SELECT
-                u.username,
-                COUNT(a.application_id) AS applications,
-                SUM(CASE WHEN a.status = 'accepted' THEN 1 ELSE 0 END) AS accepted_walks
+            SELECT u.username AS walker_username,
+                COUNT(r.rating_id) AS total_ratings,
+                ROUND(AVG(r.rating), 1) AS average_rating,
+                COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
             FROM Users u
-            LEFT JOIN WalkApplications a ON u.user_id = a.walker_id
+            LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+            LEFT JOIN WalkRequests wr ON r.request_id = wr.request_id
             WHERE u.role = 'walker'
             GROUP BY u.user_id
         `);
