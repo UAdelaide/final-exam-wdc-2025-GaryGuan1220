e5bf802755236e79b6f424c9e71ca8c80c86746f
GaryGuan1220 <guanycaa@163.com>
2025-06-20 08:03:12 +0000

    [automatic commit] 2025-06-20 08:03:12 UTC 129.127.202.63

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -5,7 +5,8 @@ const pool = require("../db");
 router.get('/', async (req, res, next) -> {
     try {
         const [rows] = await pool.query(`
-            SELECT
+            SELECT Dogs.name AS dog_name, size, Users.username AS owner
+            FROM Dogs
         `);
     } catch (error) {
 
