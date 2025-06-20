261b619f522f33d2c8a3d37856a8389bb121a4b5
GaryGuan1220 <guanycaa@163.com>
2025-06-20 08:08:18 +0000

    [automatic commit] 2025-06-20 08:08:18 UTC 129.127.202.63

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -5,7 +5,9 @@ const pool = require("../db");
 router.get('/', async (req, res, next) => {
     try {
         const [rows] = await pool.query(`
-            SELECT WalkRequests.*, Dogs.name AS dog_name
+            SELECT
+            u.username,
+            WalkRequests.*, Dogs.name AS dog_name
             FROM WalkRequests
             JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
             WHERE status = 'open'
