6aaf507818733eb8c54d1aa73b13720e4590b40c
GaryGuan1220 <guanycaa@163.com>
2025-06-20 12:10:09 +0000

    [automatic commit] 2025-06-20 12:10:09 UTC 119.15.78.5

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -5,17 +5,12 @@ const pool = require("../db");
 router.get('/', async (req, res, next) => {
     try {
         const [rows] = await pool.query(`
-            SELECT
-                wr.request_id,
-                d.name AS dog_name,
-                wr.requested_time,
-                wr.duration_minutes,
-                wr.location,
-                u.username AS owner_username
+            SELECT wr.request_id, d.name AS dog_name, wr.requested_time,
+                wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
             JOIN Dogs d ON wr.dog_id = d.dog_id
             JOIN Users u ON d.owner_id = u.user_id
-            WHERE wr.status = 'open'
+            WHERE wr.status = 'open
         `);
         res.json(rows);
     } catch (error) {
