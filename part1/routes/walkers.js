7c421b5186a0d45fee8128ae361fc68e13bee1ca
GaryGuan1220 <guanycaa@163.com>
2025-06-20 08:10:00 +0000

    [automatic commit] 2025-06-20 08:10:00 UTC 129.127.202.63

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -6,12 +6,11 @@ router.get('/', async (req, res, next) => {
     try {
         const [rows] = await pool.query(`
             SELECT
-            u.username,
-            COUNT(a.application_id) AS applications,
-            SUM(CASE WHEN a.status = 'accepted' THEN 1 ELSE 0 END) AS accepted_walks
-            WalkRequests.*, Dogs.name AS dog_name
-            FROM WalkRequests
-            JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+                u.username,
+                COUNT(a.application_id) AS applications,
+                SUM(CASE WHEN a.status = 'accepted' THEN 1 ELSE 0 END) AS accepted_walks
+            FROM Users.u
+            LEFT JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
             WHERE status = 'open'
         `);
         res.json(rows);
