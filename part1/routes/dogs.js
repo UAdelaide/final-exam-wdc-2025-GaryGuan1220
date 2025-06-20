d3b0618770c4903dd1ed01c3ad731ec6e32b9f8c
GaryGuan1220 <guanycaa@163.com>
2025-06-20 08:02:11 +0000

    [automatic commit] 2025-06-20 08:02:11 UTC 129.127.202.63

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,4 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require("../db");
 
-router.get('/', async (req, res, next))
+router.get('/', async (req, res, next) -> {
+    try {
+        
+    } catch (error) {
+
+    }
+});
