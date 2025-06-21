c475bf152c38c249fdc1c037a032c017c7915f41
GaryGuan1220 <guanycaa@163.com>
2025-06-21 06:31:03 +0000

    [automatic commit] 2025-06-21 06:31:03 UTC 119.15.78.5

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -18,9 +18,10 @@ router.get('/owner/:ownerId', async (req, res) => {
 
 router.get('/dogs', async (req, res) =>{
   try {
-    const [rows] = await db.query('')
+    const [rows] = await db.query('SELECT * FROM dogs');
+    res.json(rows);
   } catch (err) {
-
+    console.error
   }
 });
 
