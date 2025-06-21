597e26a8ffe32cd32367968506927e6c924b3e86
GaryGuan1220 <guanycaa@163.com>
2025-06-21 04:50:49 +0000

    [automatic commit] 2025-06-21 04:50:49 UTC 119.15.78.5

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,9 +3,11 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
+  password: '',
   database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
+  port: 3306,
   queueLimit: 0
 });
 
